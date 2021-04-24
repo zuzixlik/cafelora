f959fe0c29cbff679bf8c4ba8b7cabc19c76caba
Zuzana Babkova <babkova.zuzana007@gmail.com>
2021-04-24 15:01:40 +0200

    order-let-false-class-change-selected-deselected

--- src/index.js
+++ src/index.js
@@ -19,3 +19,23 @@ navAElm.forEach((element) => {
     navElm.classList.add('nav-closed');
   });
 });
+
+const orderBtnElm = document.querySelector('#order-btn');
+
+let ordered = false;
+const drinkSelected = document.querySelector('.drink__cup');
+
+orderBtnElm.addEventListener('click', (event) => {
+  console.log('drink selected');
+  console.log(drinkSelected, event.target);
+  if (ordered) {
+    ordered = false;
+    event.target.textContent = 'Objednej';
+    drinkSelected.classList.remove('drink__cup--selected');
+  } else {
+    ordered = true;
+
+    event.target.textContent = 'Zrušit';
+    drinkSelected.classList.add('drink__cup--selected');
+  }
+});
