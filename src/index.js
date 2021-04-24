a5453435f4615d0f24ddbe39073d36246676e943
Zuzana Babkova <babkova.zuzana007@gmail.com>
2021-04-24 13:06:05 +0200

    nav-click-remove class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBtnElm = document.querySelector('#nav-btn');
+
+navBtnElm.addEventListener('click', () => {
+  console.log('click');
+  const navElm = document.querySelector('nav');
+  navElm.classList.toggle('nav-closed');
+});
